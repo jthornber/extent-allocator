f3ef08b3a717215195dc8d73d5ac841414ed3e25
Ming-Hung Tsai <mtsai@redhat.com>
2023-11-16 18:56:21 +0800

    [tests] Add preallocation tests
    Also verify the number of blocks allocated

--- src/allocator/tests.rs
+++ src/allocator/tests.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{ensure, Result};
 use roaring::RoaringBitmap;
 use std::sync::{Arc, Mutex};
 
@@ -8,14 +8,14 @@ use crate::tree::utils::*;
 //----------------------------------------------------------------
 
 struct AllocationContext {
-    inner: Arc<Mutex<AllocContext>>,
+    inner: Option<Arc<Mutex<AllocContext>>>,
     blocks: Vec<u64>,
 }
 
 impl AllocationContext {
     fn new(inner: Arc<Mutex<AllocContext>>) -> Self {
         Self {
-            inner,
+            inner: Some(inner),
             blocks: Vec::new(),
         }
     }
@@ -24,7 +24,7 @@ impl AllocationContext {
     where
         F: FnMut(u64, u64) -> io::Result<Option<u64>>,
     {
-        match allocator.alloc(self.inner.clone(), f) {
+        match allocator.alloc(self.inner.as_ref().unwrap().clone(), f) {
             Ok(Some(block)) => {
                 self.blocks.push(block);
                 Ok(Some(block))
@@ -37,7 +37,7 @@ impl AllocationContext {
 
 //----------------------------------------------------------------
 
-fn _preallocate_random(allocated: &mut RoaringBitmap, count: u64, range: std::ops::Range<u64>) {
+fn preallocate_random(allocated: &mut RoaringBitmap, count: u64, range: std::ops::Range<u64>) {
     let len = range.end - range.start;
     for _ in 0..count {
         loop {
@@ -64,42 +64,140 @@ fn alloc_block(allocated: &mut RoaringBitmap, begin: u64, end: u64) -> io::Resul
     Ok(None)
 }
 
-//----------------------------------------------------------------
+fn context_alloc(
+    context: &mut AllocationContext,
+    allocator: &mut Allocator,
+    allocated: &Arc<Mutex<RoaringBitmap>>,
+) -> io::Result<Option<u64>> {
+    context.alloc(allocator, |begin, end| {
+        let mut allocated = allocated.lock().unwrap();
+        alloc_block(&mut allocated, begin, end)
+    })
+}
 
-#[test]
-fn test_prealloc_linear() -> Result<()> {
-    // Check we can handle a non-power-of-two number of blocks
-    let nr_blocks = 1024;
+fn do_allocation_test(
+    nr_blocks: u64,
+    nr_contexts: usize,
+    allocated: Arc<Mutex<RoaringBitmap>>,
+    nr_blocks_to_allocate: u64,
+) -> Result<Vec<AllocationContext>> {
     let nr_nodes = 255;
-    let nr_contexts = 16;
-    let allocated = Mutex::new(RoaringBitmap::new());
-
-    preallocate_linear(&mut allocated.lock().unwrap(), nr_blocks / 5, 100);
-
     let mut allocator = Allocator::new(nr_blocks, nr_nodes);
+
     let mut contexts = Vec::new();
     for _i in 0..nr_contexts {
         contexts.push(AllocationContext::new(allocator.get_context()));
     }
 
-    for i in 0..(nr_blocks / 2) {
-        let context = &mut contexts[(i % nr_contexts) as usize];
-        context.alloc(&mut allocator, |begin, end| {
-            let mut allocated = allocated.lock().unwrap();
-            alloc_block(&mut allocated, begin, end)
-        })?;
+    let nr_prealloc = allocated.lock().unwrap().len();
+
+    for i in 0..nr_blocks_to_allocate {
+        let context = &mut contexts[(i % nr_contexts as u64) as usize];
+        context_alloc(context, &mut allocator, &allocated)?;
     }
 
     //   dump_tree(&allocator.extents);
     //   draw_tree(&allocator.extents);
 
-    for context in contexts {
-        allocator.put_context(context.inner);
-        print_blocks(&context.blocks);
+    let mut total_nr_allocated = 0;
+    for (i, context) in contexts.iter_mut().enumerate() {
+        allocator.put_context(context.inner.take().unwrap());
+        total_nr_allocated += context.blocks.len() as u64;
+
+        // verify the number of blocks allocated per context
+        let mut expected = nr_blocks_to_allocate / nr_contexts as u64;
+        if (i as u64) < nr_blocks_to_allocate % (nr_contexts as u64) {
+            expected += 1;
+        }
+        ensure!(context.blocks.len() as u64 == expected);
     }
+
+    let nr_allocated = allocated.lock().unwrap().len();
+    ensure!(total_nr_allocated == nr_blocks_to_allocate);
+    ensure!(nr_allocated - nr_prealloc == nr_blocks_to_allocate);
 
     dump_tree(&allocator.extents);
     draw_tree(&allocator.extents);
+
+    Ok(contexts)
+}
+
+//----------------------------------------------------------------
+
+// TODO: Check we can handle a non-power-of-two number of blocks
+
+#[test]
+fn test_prealloc_linear_start() -> Result<()> {
+    let nr_blocks = 1024;
+    let nr_contexts = 16;
+    let allocated = Arc::new(Mutex::new(RoaringBitmap::new()));
+    let nr_prealloc = nr_blocks / 5;
+
+    preallocate_linear(&mut allocated.lock().unwrap(), nr_prealloc, 0);
+
+    let contexts = do_allocation_test(nr_blocks, nr_contexts, allocated, nr_blocks / 2)?;
+
+    for context in contexts {
+        print_blocks(&context.blocks);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_prealloc_linear_middle() -> Result<()> {
+    let nr_blocks = 1024;
+    let nr_contexts = 16;
+    let allocated = Arc::new(Mutex::new(RoaringBitmap::new()));
+    let nr_prealloc = nr_blocks / 5;
+
+    preallocate_linear(&mut allocated.lock().unwrap(), nr_prealloc, 100);
+
+    let contexts = do_allocation_test(nr_blocks, nr_contexts, allocated, nr_blocks / 2)?;
+
+    for context in contexts {
+        print_blocks(&context.blocks);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_prealloc_linear_end() -> Result<()> {
+    let nr_blocks = 1024;
+    let nr_contexts = 16;
+    let allocated = Arc::new(Mutex::new(RoaringBitmap::new()));
+    let nr_prealloc = nr_blocks / 5;
+
+    preallocate_linear(
+        &mut allocated.lock().unwrap(),
+        nr_prealloc,
+        nr_blocks - nr_prealloc,
+    );
+
+    let contexts = do_allocation_test(nr_blocks, nr_contexts, allocated, nr_blocks / 2)?;
+
+    for context in contexts {
+        print_blocks(&context.blocks);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_prealloc_random() -> Result<()> {
+    let nr_blocks = 1024;
+    let nr_contexts = 16;
+    let allocated = Arc::new(Mutex::new(RoaringBitmap::new()));
+    let nr_prealloc = nr_blocks / 5;
+
+    preallocate_random(&mut allocated.lock().unwrap(), nr_prealloc, 0..nr_blocks);
+
+    let contexts = do_allocation_test(nr_blocks, nr_contexts, allocated, nr_blocks / 2)?;
+
+    for context in contexts {
+        print_blocks(&context.blocks);
+    }
 
     Ok(())
 }
